a8ed8b92e3a14acf69c4abed58305e265c501fe0
Chase-Coudret <chasecoudret@cromwell.school.nz>
2025-05-19 11:59:29 +1200

    Add files via upload

---
+++ Street Racer Working Name and Icon.py
@@ -0,0 +1,6 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\coudr\Downloads\New folder\python\Street Racer\Street Racer.py
+pygame 2.6.1 (SDL 2.28.4, Python 3.13.1)
+Hello from the pygame community. https://www.pygame.org/contribute.html
